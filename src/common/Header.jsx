0265e0666d19fa212d903fb8e7d3dcb06ec4db1e
Sheldon Debra <kofisheldon@gmail.com>
2024-06-11 19:31:42 +0000

    create Header

---
+++ src/common/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <h1>Sheldon Debra</h1>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
